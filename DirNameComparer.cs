1d820c042055503b4a48e8efb0a97d1ab77e4f92
Louis <louis@falkknudsen.com>
2025-10-16 01:09:39 +0200

    Implemented a class which compares DirectoryInfos and path strings based on only the DirectoryInfo.Name part.

---
+++ DirNameComparer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.IO;
+
+namespace FindDuplicateDirs;
+
+public class DirNameComparer : IEqualityComparer, IEqualityComparer<string>, IEqualityComparer<DirectoryInfo> {
+    private static DirNameComparer? _instance;
+    public static DirNameComparer Instance => _instance ??= new DirNameComparer();
+    
+    public new bool Equals(object? x, object? y) {
+        if (x is string xDir && y is string yDir) {
+            return Path.GetFileName(xDir) == Path.GetFileName(yDir);
+        }
+        return false;
+    }
+
+    int IEqualityComparer.GetHashCode(object obj) {
+        switch (obj) {
+            case string stringObj:
+                return Path.GetFileName(stringObj).GetHashCode();
+            case DirectoryInfo dir:
+                return dir.Name.GetHashCode();
+            default:
+                return obj.GetHashCode();
+        }
+    }
+
+    public bool Equals(string? x, string? y) {
+        if (x is null || y is null) return false;
+        return Path.GetFileName(x) == Path.GetFileName(y);
+    }
+
+    public int GetHashCode(string obj) {
+        return Path.GetFileName(obj).GetHashCode();
+    }
+
+    public bool Equals(DirectoryInfo? x, DirectoryInfo? y) {
+        if (x is null || y is null) return false;
+        return x.Name == y.Name;
+    }
+
+    public int GetHashCode(DirectoryInfo obj) {
+        return obj.Name.GetHashCode();
+    }
+}
