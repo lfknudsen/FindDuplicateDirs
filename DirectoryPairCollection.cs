a0a5706a9e3bfa9e5d817e0a57f0d6956ad7efe7
Louis <louis@falkknudsen.com>
2025-10-16 01:05:42 +0200

    Created new Collection of DirectoryInfo tuples to hold the output

---
+++ DirectoryPairCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using Tommy;
+
+namespace FindDuplicateDirs;
+
+public class DirectoryPairCollection : ObservableCollection<Tuple<DirectoryInfo, DirectoryInfo>> {
+    public void Add(IEnumerable<Tuple<DirectoryInfo, DirectoryInfo>> items) {
+        foreach (Tuple<DirectoryInfo, DirectoryInfo> item in items) {
+            Add(item);
+        }
+    }
+}
