dbc979a715d078f963619a2fa85ada9f17b665ba
Louis <louis@falkknudsen.com>
2025-10-15 21:23:50 +0200

    Extended the DirectoryEntry collection to be able to automatically add a TOML array, as well as to return its values in TOML format as well.

--- DirectoryCollection.cs
+++ DirectoryCollection.cs
@@ -1,7 +1,31 @@
 ﻿using System.Collections.ObjectModel;
 using System.DirectoryServices;
+using System.IO;
+using Tommy;
 
 namespace FindDuplicateDirs;
 
 public class DirectoryCollection : ObservableCollection<DirectoryEntry> {
+    public void AddTomlArray(TomlArray array) {
+        foreach (TomlNode entry in array) {
+            if (entry.IsString) {
+                string dirPath = entry.AsString.Value;
+                if (Directory.Exists(dirPath)) {
+                    Add(new DirectoryEntry(dirPath));
+                }
+            }
+        }
+    }
+
+    public IEnumerable<TomlString> MarshallToml() {
+        return this.Select(entry => new TomlString {
+            Value = entry.Path
+        });
+    }
+
+    public TomlArray ToTomlArray() {
+        var arr = new TomlArray();
+        arr.AddRange(MarshallToml());
+        return arr;
+    }
 }
