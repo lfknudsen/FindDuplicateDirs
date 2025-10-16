3d4ce39ee4f393224bf4145791d5c388f999f9ba
Louis <louis@falkknudsen.com>
2025-10-16 16:24:43 +0200

    Added comments to everything in MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using Microsoft.Win32;
@@ -21,34 +20,56 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         SetBtnStartState(DirList);
     }
 
+    /** Is this instance subscribed to change events on <see cref="DirList"/>? */
     private bool _subscribed = false;
 
+    /** Print extraneous information to the standard output while running? */
     public const bool VERBOSE = true;
 
-    /** When the folder find dialogue is opened, this is where it starts. */
+    /** When a folder choosing dialogue is opened, this is where it starts. */
     private string _initialDirectory = Environment.CurrentDirectory;
 
+    /** Complete path to the directory wherein the config file is stored. */
     private static readonly string CONFIG_DIRECTORY = Path.Combine(
         Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FindDuplicateDirs");
 
+    /** Complete path to the configuration file. */
     private static readonly string CONFIG_FILENAME = Path.Combine(CONFIG_DIRECTORY, "config.toml");
 
+    /** TOML key for the default folder chooser directory. */
     private const string CONFIG_DEFAULT_DIR = "default_dir";
+    /** TOML key for the array of most recent input directories. */
     private const string CONFIG_LAST_DIR_LIST = "last_dir_list";
+    
+    /** String which identifies the <see cref="StaticResourceExtension">static resource</see>
+     * <see cref="DirectoryCollection"/>.*/
     private const string DirResource = "DirList";
+    
+    /** String which identifies the <see cref="StaticResourceExtension">static resource</see>
+     * <see cref="DirectoryPairCollection"/>.*/
     private const string DupListResource = "DuplicateList";
 
+    /** The <see cref="DirectoryCollection"/> resource which backs the <see cref="ListView"/> on the left-hand
+     * side of the programme. It contains the top directories to search through. */
     private DirectoryCollection? DirList {
         get { return field ??= TryFindResource(DirResource) as DirectoryCollection; }
     }
 
+    /** The <see cref="DirectoryPairCollection"/> resource which backs the <see cref="ListView"/> on the right-hand
+     * side of the programme. It contains the child directories which have a non-unique
+     * name across all input directories. */
     private DirectoryPairCollection? DuplicateList {
         get { return field ??= TryFindResource(DupListResource) as DirectoryPairCollection; }
     }
 
-    /** Null = all */
+    /** Limits the number of directories written to the config file to be
+     * re-opened next time the programme is started up.
+     * <remarks> null means to write everything </remarks> */
     private static readonly int? MAX_RECENT_DIRS_TO_SAVE = null;
 
+    /** Displays a standard file explorer dialogue to the user which
+     * allows them to select however many directories they wish.
+     * The user's selection is appended to the <see cref="DirList"/>. */
     private int SelectDirectories() {
         EnsureSubscribed();
         var dialogue = new OpenFolderDialog {
@@ -79,16 +100,23 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         return DirList.Count;
     }
 
+    /** Function called when the user clicks the button to select directories
+     * to add to the <see cref="DirList"/>. */
     private void SelectDirs_OnClick(object sender, RoutedEventArgs e) {
         SelectDirectories();
     }
 
+    /** Function called when the user clicks the button to clear the contents of the
+     * <see cref="DirList"/>. */
     private void Clear_OnClick(object sender, RoutedEventArgs e) {
         EnsureSubscribed();
         DirList?.Clear();
         SaveConfig();
     }
 
+    /** Opens a folder chooser dialogue allowing the user to select a single folder.
+     From this point on, it becomes the starting directory when the user opens
+     a folder dialogue through the programme. */
     private string SetInitialDirectory() {
         var dialogue = new OpenFolderDialog {
             Multiselect = false,
@@ -106,10 +134,17 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         return _initialDirectory;
     }
 
+    /** Function called when the user calls the function to change the default
+     * initial directory which the file chooser opens into when clicking the
+     * button to select directories to add to the list. */
     private void SetInitialDir_OnClick(object sender, RoutedEventArgs e) {
         SetInitialDirectory();
     }
 
+    /** Function called when the user clicks the <see cref="BtnStart">"start"</see> button.
+     * First removes all duplicates from the <see cref="DirList"/>, then
+     * proceeds to fill the <see cref="DuplicateList"/> with tuples of
+     * subdirectories with identical names but different paths. */
     private void Activate_OnClick(object sender, RoutedEventArgs e) {
         EnsureSubscribed();
         if (DirList is null || DirList.Count < 2) {
@@ -120,11 +155,16 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
 
         DuplicateList?.Clear();
         DirList?.RemoveDuplicates();
-        DuplicateList?.Add(DupDirectories());
+        DuplicateList?.Add(FindDupDirectories());
     }
 
-    private IEnumerable<Tuple<DirectoryInfo, DirectoryInfo>> DupDirectories() {
-        EnsureSubscribed();
+    /** Yields a collection of pairs of directories with identical names
+     * but different paths.<br/>
+     * This function is not recursive; it <em>only</em> looks for
+     * <see cref="DirectoryInfo.EnumerateDirectories()">immediate children</see> of
+     * the directories in the <see cref="DirList"/>.
+     */
+    private IEnumerable<Tuple<DirectoryInfo, DirectoryInfo>> FindDupDirectories() {
         if (DirList == null || DirList.Count < 2 || DuplicateList == null) yield break;
 
         var hashes = new HashSet<DirectoryInfo>(DirNameComparer.Instance);
@@ -143,9 +183,16 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         }
     }
 
+    /** Loads the configuration file at path <c>.AppData/Roaming/FindDuplicateDirs/config.toml</c>
+     * if this exists.<br/><br/>
+     * Two bits of information are stored within:
+     * <list type="number">
+     * <item><see cref="_initialDirectory">Default directory</see> for the folder chooser dialogues</item>
+     * <item>The contents of the <see cref="DirList"/> last the programme was used.</item>
+     * </list>
+     * The information is automatically saved to this file whenever either is changed
+     * in the programme. */
     private void LoadConfig() {
-        EnsureSubscribed();
-        
         if (!Directory.Exists(CONFIG_DIRECTORY)) {
             Directory.CreateDirectory(CONFIG_DIRECTORY);
             return;
@@ -191,9 +238,15 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         Dispose();
     }
 
+    /** Save information to the user's configuration file in
+     * <c>.AppData/Roaming/FindDuplicateDirs/config.toml</c>.
+     * Called whenever the <see cref="DirList"/> is updated by this class.
+     * The following information is stored:
+     * <list type="number">
+     * <item><see cref="_initialDirectory">Default directory</see> for the folder chooser dialogues</item>
+     * <item>The absolute paths of the contents of the <see cref="DirList"/>.</item>
+     * </list>*/
     private void SaveConfig() {
-        EnsureSubscribed();
-        
         if (!Directory.Exists(CONFIG_DIRECTORY)) {
             Directory.CreateDirectory(CONFIG_DIRECTORY);
         }
@@ -209,6 +262,9 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         }
     }
 
+    /** Writes the absolute path of the current
+     * <see cref="_initialDirectory">default directory</see> (when
+     * opening a folder dialogue) to the config file. */
     private void WriteInitialDirectory(StreamWriter writer) {
         if (_initialDirectory != Environment.CurrentDirectory) {
             var tomlString = new TomlString {
@@ -220,7 +276,9 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
             writer.Flush();
         }
     }
-
+    
+    /** Writes the absolute paths of the current contents of the 
+    <see cref="DirList"/> to the config file as an <see cref="TomlArray">array</see>. */
     private void WriteRecentDirectories(StreamWriter writer) {
         if (DirList != null && DirList.Count > 0) {
             var arr = DirList.ToTomlArray(MAX_RECENT_DIRS_TO_SAVE);
@@ -231,12 +289,17 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         }
     }
 
+    /** Delete the config file. Called when an error occurred
+     * while attempting to parse the file. */
     private static void ClearConfigFile() {
         if (File.Exists(CONFIG_FILENAME)) {
             File.Delete(CONFIG_FILENAME);
         }
     }
 
+    /** Function called when the user has clicked an option in the <see cref="ContextMenu">context menu</see>
+     * for an <see cref="ListViewItem">item</see> in the <see cref="ListView"/> on the left side of the application (i.e. the
+     * view of the <see cref="DirList"/>). */
     private void DirItem_OnRightClick(object sender, RoutedEventArgs e) {
         EnsureSubscribed();
         var item = e.Source as MenuItem;
@@ -248,6 +311,9 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         }
     }
 
+    /** Function called when the user has clicked an option in the <see cref="ContextMenu">context menu</see>
+     * for an <see cref="ListViewItem">item</see> in the <see cref="ListView"/> on the right side of the application (i.e. the
+     * view of the <see cref="DuplicateList"/>). */
     private void DupItem_OnRightClick(object sender, RoutedEventArgs e) {
         EnsureSubscribed();
         var item = e.Source as MenuItem;
@@ -258,7 +324,7 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         }
     }
 
-    /** If this instance isn't subscribed to DirList, attempt to subscribe. */
+    /** Attempts to subscribe to the <see cref="DirList"/> if it isn't already. */
     private void EnsureSubscribed() {
         if (!_subscribed) {
             _subscribed = DirList?.Subscribe(this) != null;
@@ -273,13 +339,17 @@ public partial class MainWindow : Window, IDisposable, IObserver<DirectoryCollec
         // Irrelevant
     }
 
-    /** When the DirList is modified, this function is called.
+    /** <inheritdoc cref="IObserver{DirectoryCollection}.OnNext"/><br/><br/>
+     When the DirList is modified, this function is called.
      Disables the activation button if the number of elements in
-     the DirectoryCollection is less than 2, and vice versa. */
+     the <see cref="DirectoryCollection"/> is less than 2, and vice versa. */
     public void OnNext(DirectoryCollection value) {
         SetBtnStartState(value);
     }
     
+    /** Sets the <c>IsEnabled</c> flag of the button which starts the duplication-finding process.
+     * If the input <see cref="DirectoryCollection"/> is null or has fewer than 2 elements in it, then
+     * the button is disabled. Otherwise, it is enabled. */
     [SuppressMessage("Performance", "CA1822:Mark members as static")] // Incorrect. BtnStart is non-static.
     private void SetBtnStartState(DirectoryCollection? value) {
         if (value == null) {
