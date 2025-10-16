a12233bee7b2528f79bcc112d5ca2b91d5c685ad
Louis <louis@falkknudsen.com>
2025-10-16 17:45:19 +0200

    Implemented extension methods to get the size (in bytes) of DirectoryInfo instances.

---
+++ DirInfoExtensions.cs
@@ -0,0 +1,23 @@
+using System.IO;
+
+namespace FindDuplicateDirs;
+
+public static class DirInfoExtensions {
+    public static long Size(this DirectoryInfo dir) {
+        if (!dir.Exists) return 0;
+        return dir.EnumerateFiles().Aggregate<FileInfo, long>(0, Acc<long, FileInfo>)
+            + dir.EnumerateDirectories().Aggregate<DirectoryInfo, long>(0, Acc<long, DirectoryInfo>);
+    }
+
+    public static long Size(this FileInfo file) {
+        return file.Exists ? file.Length : 0;
+    }
+
+    private static long Acc<TAcc, T>(long seed, FileInfo element) {
+        return seed + element.Size();
+    }
+
+    private static long Acc<TAcc, T>(long seed, DirectoryInfo element) {
+        return seed + element.Size();
+    }
+}
