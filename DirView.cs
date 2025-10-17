8add2c041e2d71488dc012a5f01896fc7eaa0b7f
Louis <louis@falkknudsen.com>
2025-10-17 23:02:21 +0200

    Created a "view" wrapper of the DirectoryInfo class, which can allow me to include directory size information in the output list, and hopefully to compute asynchronously.

---
+++ DirView.cs
@@ -0,0 +1,41 @@
+using System.IO;
+
+namespace FindDuplicateDirs;
+
+public class DirView {
+    private readonly DirectoryInfo _dir;
+
+    public string FullName => _dir.FullName;
+
+    public long Size { get; private set; } = -1;
+
+    public string DirSize { get; private set; } = "";
+
+    private readonly Task _computeSizeTask;
+
+
+    public DirView(DirectoryInfo dir) {
+        _dir = dir;
+        _computeSizeTask = Task.Run(ComputeSize);
+    }
+
+    public DirView(string path) : this(new DirectoryInfo(path)) { }
+
+    private void ComputeSize() {
+        if (!_dir.Exists)
+            return;
+        Size = _dir.EnumerateFiles()
+                   .Aggregate<FileInfo, long>(0, Acc<FileInfo>)
+               + _dir.EnumerateDirectories()
+                   .Aggregate<DirectoryInfo, long>(0, Acc<DirectoryInfo>);
+        DirSize = Size.ToString();
+    }
+
+    private static long Acc<T>(long seed, FileInfo element) {
+        return seed + element.Size();
+    }
+
+    private static long Acc<T>(long seed, DirectoryInfo element) {
+        return seed + element.Size();
+    }
+}
